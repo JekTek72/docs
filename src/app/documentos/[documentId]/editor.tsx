58567be2b9e1aa45efed3b3b5191cb9fd59e1087
JekTek <juanvicentebio653@gmail.com>
2024-12-08 15:28:19 +0100

    Font Family & Headings Tools

--- src/app/documentos/[documentId]/editor.tsx
+++ src/app/documentos/[documentId]/editor.tsx
@@ -12,6 +12,8 @@ import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
 import { useEditorStore } from '@/store/use-editor-store';
 import Underline from '@tiptap/extension-underline';
+import FontFamily from '@tiptap/extension-font-family';
+import TextStyle from '@tiptap/extension-text-style'
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
@@ -60,6 +62,8 @@ export const Editor = () => {
       Image,
       ImageResize,
       Underline,
+      FontFamily,
+      TextStyle,
     ],
     content: `
       <table>
